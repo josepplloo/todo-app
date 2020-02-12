a779b78846e752173c285139581dc7d339fbf517
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2020-02-11 22:39:12 -0500

    adding input element and some global css styles

--- src/components/App/createApp.js
+++ src/components/App/createApp.js
@@ -1,8 +1,13 @@
 import createTitle from '../Title/createTitle';
+import createInput from '../Input/createInput';
+import './app.scss';
+
 
 function createApp(node) {
   const titleElement = createTitle();
+  const mainImputElement = createInput();
   node.appendChild(titleElement);
+  node.appendChild(mainImputElement);
 }
 
 export default createApp;
