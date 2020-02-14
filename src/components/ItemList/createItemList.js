140cd146af415c0c35bfc7c75bb3f582fa8b26a3
Jose Miguel Garcia garcia <jose.garcia@wolox.co>
2020-02-14 14:44:34 -0500

    Testing the new item Component

--- src/components/ItemList/createItemList.js
+++ src/components/ItemList/createItemList.js
@@ -1,12 +1,13 @@
 import constants from './constants';
 import './itemList.scss';
 import itemData from '../../data/data';
+import ceateTodoItem from '../TodoItem/createTodoItem';
 
 function createitemList() {
   const itemListElement = document.createElement('UL');
   itemListElement.setAttribute('label', constants.altText);
-  itemListElement.innerHTML = JSON.stringify(itemData);
   itemListElement.className = 'app-item-list';
+  itemListElement.innerHTML = itemData.map((item, index) => `<li key=${index}>${ceateTodoItem(item)}</li>`);
   return itemListElement;
 }
 
--- src/components/ItemList/createItemList.test.js
+++ src/components/ItemList/createItemList.test.js
@@ -5,12 +5,7 @@ describe('test for ItemList component', () => {
     const itemListElement = createItemList();
     expect(itemListElement.tagName).toBe('UL');
   });
-
-  it.skip('should have "todo" as title value', () => {
-    const itemListElement = createItemList();
-    expect(itemListElement.innerHTML).toBe('todos');
-  });
-  it.skip('should be like the snaptshot', () => {
+  it('should be like the snaptshot', () => {
     const itemListElement = createItemList();
     expect(itemListElement).toMatchSnapshot();
   });
