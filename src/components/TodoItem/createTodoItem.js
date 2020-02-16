241ee0e61d5723d1c1e7c8f21c2d3202f6a826cc
josepplloo <josepplloo@gmail.com>
2020-02-16 14:00:26 -0500

    creating the 2/3 components inside todo item

--- src/components/TodoItem/createTodoItem.js
+++ src/components/TodoItem/createTodoItem.js
@@ -1,7 +1,13 @@
+import createToggleButton from './components/createToggleButton';
+import createEditableLabel from './components/createEditableLabel';
+import './todoItem.scss';
+
 function createTodoItem({ itemName, itemStatus } = { itemName: '', itemStatus: false }, index = 0) {
   const todoElement = document.createElement('li');
   todoElement.setAttribute('data-key', index);
-  todoElement.innerHTML = `${itemStatus} ${itemName}`;
+  todoElement.appendChild(createToggleButton(itemStatus));
+  todoElement.appendChild(createEditableLabel(itemName));
+  todoElement.className = 'todoitem';
   return todoElement;
 }
 
