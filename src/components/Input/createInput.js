c4504513339983b9b2960041ecf6dcb8b7a8c2bb
josepplloo <josepplloo@gmail.com>
2020-02-19 21:42:09 -0500

    handling input data

--- src/components/Input/createInput.js
+++ src/components/Input/createInput.js
@@ -6,7 +6,21 @@ function createInput() {
   inputElement.setAttribute('label', constants.altText);
   inputElement.setAttribute('placeholder', constants.placeholderText);
   inputElement.className = 'app-input';
+  inputElement.handleInput = (inputText = inputElement.value) => {
+    // TODO: send the value to data object.
+    console.log(inputText);
+    return {
+      itemName: inputText,
+      itemStatus: false,
+    };
+  };
 
+  inputElement.onkeyup = (e) => {
+    if (e.code === 'Enter') {
+      inputElement.handleInput();
+      inputElement.value = '';
+    }
+  };
   return inputElement;
 }
 
